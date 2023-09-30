f06914bd5ab550fc2447341f95753d579535c802
skakizaki <shohachi.kakizaki@gmail.com>
2023-09-30 19:27:15 +0900

    add Word

--- src/saveAsPdf.ts
+++ src/saveAsPdf.ts
@@ -9,6 +9,28 @@ const xlQualityStandard = 0;
 const xlQualityMinimum = 1;
 const xlQuality = xlQualityStandard;
 
+const wdExportFormatPDF = 17;  // PDF
+
+export function saveWord(data: fo.Document[], workspaceDir: string, wordRelpath: string) {
+    const wordPath = path.resolve(path.join(workspaceDir, wordRelpath));
+    var pdfPath = path.join(workspaceDir, ".mPDF", wordRelpath);
+    var basename = path.basename(pdfPath);
+    var name = path.parse(basename).name;
+    pdfPath = path.join(
+        path.dirname(pdfPath),
+        `${name}.pdf`
+    );
+    console.log("convert from ", wordPath);
+    console.log("convert to   ", pdfPath);
+
+    const wordApplication = new winax.Object("Word.Application", { activate: true});
+    const wordDocument = wordApplication.Documents.Open(wordPath, 0, true, true);
+    fs.mkdirSync(path.dirname(pdfPath), { recursive: true });
+    wordDocument.ExportAsFixedFormat(pdfPath, wdExportFormatPDF);
+    wordDocument.Close();
+    wordApplication.Quit();
+}
+
 export function saveExcel(data: fo.Document[], workspaceDir: string, xlsRelpath: string) {
     const xlsPath = path.resolve(path.join(workspaceDir, xlsRelpath));
     var pdfPath = path.join(workspaceDir, ".mPDF", xlsRelpath);
@@ -18,14 +40,14 @@ export function saveExcel(data: fo.Document[], workspaceDir: string, xlsRelpath:
         path.dirname(pdfPath),
         `${name}.pdf`
     );
-    console.log("convert from ", xlsPath, " to ", pdfPath);
+    console.log("convert from ", xlsPath);
+    console.log("convert to   ", pdfPath);
 
     const excel = new winax.Object("Excel.Application", { activate: true});
     const workbooks = excel.Workbooks;
     const workbook = workbooks.Open(xlsPath, 0, true);
     const doc = data.find((doc) => doc.name === xlsRelpath);
     const sheets = doc?.worksheets?.filter((sheetname) => sheetname.visible);
-//    console.log(sheets);
 
     var replace = true;
     sheets?.forEach((e) => {
@@ -46,6 +68,9 @@ export function savePdf(jsonData: fo.Document[], workspaceDir: string, documentU
     switch(ext) {
         case ".xlsx":
             saveExcel(jsonData, workspaceDir ,document);
+            break;
+        case ".docx":
+            saveWord(jsonData, workspaceDir, document);
             break;
         default:
             break;
